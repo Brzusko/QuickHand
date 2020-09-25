f0a3dcd092a43781f19baccf66ced74a8942c28d
KingDragomon <digimon@vp.pl>
2020-09-25 18:59:42 +0200

    Main_Menu
    Add Main Menu Scene

---
+++ Scenes/Main_Menu.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+func _on_Play_pressed():
+	#get_tree().change_scene("");
+	pass
+
+
+func _on_Settings_pressed():
+	#get_tree().change_scene("");
+	pass
+
+
+func _on_Exit_pressed():
+	get_tree().quit();
+	pass 
