6ddf31ac1c9d7e937a1a26c16a3eea43ca4f60e3
KingDragomon <digimon@vp.pl>
2020-10-01 17:27:33 +0200

    Json to dictionary

--- Autoload/HTTP_Request.gd
+++ Autoload/HTTP_Request.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 func make_request():
-	$HTTPRequest.request("https://jsonplaceholder.typicode.com/todos/1");
+	$HTTPRequest.request("http://52.169.226.95/servers/QuickHand");
 	pass
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if(response_code == 200):
-		var Jason = body.get_string_from_utf8();
+		var dict = {}
+		dict = parse_json(body.get_string_from_utf8());
 	pass
