1b678b854b1dc2495e644bd4e3fcd0d602e520ec
KingDragomon <digimon@vp.pl>
2020-10-02 17:44:19 +0200

    testing json errors

--- Autoload/HTTP_Request.gd
+++ Autoload/HTTP_Request.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 func make_request():
-	$HTTPRequest.request("http://52.169.226.95/servers/QuickHand");
+	$HTTPRequest.request("http://52.169.226.95/servers/QuickHand",[],false,HTTPClient.METHOD_GET);
 	pass
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if(response_code == 200):
 		var dict = {}
-		dict = parse_json(body.get_string_from_utf8());
+		dict = JSON.parse(body.get_string_from_utf8());
+		if (dict.error != OK):
+			print("Something goes wrong with parsing data");
+			return;
+		#print(dict.result);
 	pass
