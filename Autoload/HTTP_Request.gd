86270a72d5ab3d77068ff79c29082fcd3cce608a
KingDragomon <digimon@vp.pl>
2020-10-03 18:38:42 +0200

    Server ping function

--- Autoload/HTTP_Request.gd
+++ Autoload/HTTP_Request.gd
@@ -1,21 +1,43 @@
 extends Node2D
 
 #Server registration
-func register_server():
+func register_server(address,port,s_name,players,count):
+	
+	#Data to dictionary
 	var data = {
-		"address": "127.0.0.1",
-		"port": 7171,
-		"server_name": "Maro testing",
-		"max_players": 2,
-		"max_count": 3,
+		"address": address,
+		"port": port,
+		"server_name": s_name,
+		"max_players": players,
+		"max_count": count,
 	}
 	
+	#Dictionary to json
 	var query = JSON.print(data);
+	
+	#Sending request
 	$Server_Registration.request("http://52.169.226.95/servers/register/QuickHand",["Content-Type: application/json"],false,HTTPClient.METHOD_POST,query);
 	pass
 
 func _on_Server_Registration_request_completed(result, response_code, headers, body):
-	print(body.get_string_from_utf8());
+	
+	#Json to dictionary
+	var dict = {}
+	dict = JSON.parse(body.get_string_from_utf8());
+	
+	#Json error test
+	if (dict.error != OK):
+		NotificationNode.show_notification("Something goes wrong with parsing data",10,0);
+		return;
+	
+	#Notification from server
+	match dict.result["message"]:
+		"ADDED":
+			NotificationNode.show_notification("Succesfuly registered server",10,2);
+		"FAILED_TO_ADD":
+			NotificationNode.show_notification("Failed to register server",10,0);
+		"MASTER_SERVER_NOT_FOUND":
+			NotificationNode.show_notification("Master server not found",10,0);
 	pass
 
 
@@ -32,8 +54,21 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 		if (dict.error != OK):
 			print("Something goes wrong with parsing data");
 			return;
-		#print(dict.result);
+		print(dict.result);
 	pass
 
 
 
+
+func server_ping(ip,port,players):
+	
+	#Players dictionary to json
+	var query = JSON.print(players);
+	
+	#Sending request
+	$Server_Registration.request("http://52.169.226.95/servers/update_server/QuickHand/"+ip+"/"+port,["Content-Type: application/json"],false,HTTPClient.METHOD_POST,query);
+	pass;
+
+func _on_Server_Ping_request_completed(result, response_code, headers, body):
+	
+	pass
